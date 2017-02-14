310239de86d306d6a00749053a2dc85d71e2cd2b
madison kanna <madisonkanna@gmail.com>
2017-02-14 14:13:46 -0800

    Add titles

--- js/app.js
+++ js/app.js
@@ -21,7 +21,21 @@ var Dalmatian = function() {
 	//Add increment counter
 	this.nicknames = ko.observableArray(['Cutie', 'Dotty', 'Bug', 'Spots']);
 
-	
-
+	this.title = ko.computed(function() {
+		var tite;
+		var clicks = this.clickCount() {
+			if (clicks < 10) {
+				title = 'Newborn';
+			} else if (clicks < 50) {
+				title = 'Infant';
+			} else if (clicks < 100) {
+				title = 'Child';
+			} else if (clicks < 500) {
+				title = 'Adult';
+			} else {
+				title = 'Ninja';
+			}
+			return title;
+		}, this);
 
 }
